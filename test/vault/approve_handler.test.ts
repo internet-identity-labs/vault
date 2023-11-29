10d6865ac6104859234e3c0b18ec35de4197643c
olsemeno <sendtosemenov@gmail.com>
2023-11-29 14:52:36 +0100

    Approves tests

--- test/vault/approve_handler.test.ts
+++ test/vault/approve_handler.test.ts
@@ -15,9 +15,11 @@ describe("Approve Handler Transactions", () => {
     let admin_identity1 = getIdentity("87654321876543218765432187654321")
     let admin_identity2 = getIdentity("87654321876543218765432187654322")
     let admin_identity3 = getIdentity("87654321876543218765432187654323")
+    let member_identity4 = getIdentity("87654321876543218765432187654325")
     let manager1: VaultManager;
     let manager2: VaultManager;
     let manager3: VaultManager;
+    let memberManager4: VaultManager;
     before(async () => {
         DFX.INIT();
         DFX.USE_TEST_ADMIN();
@@ -26,12 +28,15 @@ describe("Approve Handler Transactions", () => {
         manager1 = new VaultManager();
         manager2 = new VaultManager();
         manager3 = new VaultManager();
+        memberManager4 = new VaultManager();
         await manager1.init(canister_id, admin_identity1, true);
         await manager2.init(canister_id, admin_identity2, true);
         await manager3.init(canister_id, admin_identity3, true);
+        await memberManager4.init(canister_id, member_identity4, true);
         await requestCreateMemberTransaction(manager1, principalToAddress(admin_identity2.getPrincipal() as any), "a2", VaultRole.ADMIN);
         await requestCreateMemberTransaction(manager1, principalToAddress(admin_identity3.getPrincipal() as any), "a3", VaultRole.ADMIN);
-        await requestUpdateQuorumTransaction(manager1, 3);
+        await requestCreateMemberTransaction(manager1, principalToAddress(member_identity4.getPrincipal() as any), "m3", VaultRole.MEMBER);
+        await requestUpdateQuorumTransaction(manager1, 2);
         await manager1.execute()
     });
 
@@ -39,8 +44,46 @@ describe("Approve Handler Transactions", () => {
         DFX.STOP();
     });
 
+
+    it("Trs approved by 1 and rejected by 2", async function () {
+        let tr_id = (await requestUpdateQuorumTransaction(manager1, 3))[0].id
+        let reject: ApproveRequest = {
+            tr_id,
+            state: TransactionState.Rejected
+        }
+        await manager2.approveTransaction([reject])
+        let tr = (await getTransactionByIdFromGetAllTrs(manager1, tr_id));
+        expect(tr.approves.length).eq(2);
+        expect(tr.state).eq(TransactionState.Pending);
+
+        await manager3.approveTransaction([reject])
+        tr = (await getTransactionByIdFromGetAllTrs(manager1, tr_id));
+        expect(tr.approves.length).eq(3);
+        expect(tr.state).eq(TransactionState.Rejected);
+
+        await manager1.execute();
+        tr = (await getTransactionByIdFromGetAllTrs(manager1, tr_id));
+        expect(tr.state).eq(TransactionState.Rejected);
+    });
+
+    it("Trs approved by 2 and rejected by 1", async function () {
+        let tr_id = (await requestUpdateQuorumTransaction(manager1, 3))[0].id
+        let approve: ApproveRequest = {
+            tr_id,
+            state: TransactionState.Approved
+        }
+        await manager2.approveTransaction([approve])
+        let tr = (await getTransactionByIdFromGetAllTrs(manager1, tr_id));
+        expect(tr.approves.length).eq(2);
+        expect(tr.state).eq(TransactionState.Approved);
+
+        await manager1.execute();
+        tr = (await getTransactionByIdFromGetAllTrs(manager1, tr_id));
+        expect(tr.state).eq(TransactionState.Executed);
+    });
+
     it("Trs approved by 3 and executed", async function () {
-        let tr_id = (await requestCreateWalletTransaction(manager1, "1", Network.IC))[0].id
+        let tr_id = (await requestCreateWalletTransaction(manager1, "2", Network.IC))[0].id
         let approve: ApproveRequest = {
             tr_id,
             state: TransactionState.Approved
@@ -49,6 +92,12 @@ describe("Approve Handler Transactions", () => {
         let tr = (await getTransactionByIdFromGetAllTrs(manager1, tr_id));
         expect(tr.approves.length).eq(2);
         expect(tr.state).eq(TransactionState.Pending);
+
+        try {
+            await memberManager4.approveTransaction([approve])
+        } catch (e) {
+            expect(e.message).contains("Not permitted")
+        }
 
         try {
             await manager1.approveTransaction([approve])
@@ -74,7 +123,7 @@ describe("Approve Handler Transactions", () => {
         expect(tr.state).eq(TransactionState.Executed);
     });
 
-    it("Trs rejected by 2 and rejected", async function () {
+    it("Trs rejected by 1 from 3 and rejected", async function () {
         let tr_id = (await requestCreateWalletTransaction(manager1, "1", Network.IC))[0].id
         let approve: ApproveRequest = {
             tr_id,
