6ff60137a7072b274846ca016b773df9520f3a09
olsemeno <sendtosemenov@gmail.com>
2023-12-01 15:09:21 +0100

    Batch test

--- test/vault/barch_transaction.test.ts
+++ test/vault/barch_transaction.test.ts
@@ -1,28 +1,36 @@
 import {DFX} from "../constanst/dfx.const";
 import {getIdentity} from "../util/deployment.util";
 import {
+    ApproveRequest,
     MemberCreateTransactionRequest,
-    QuorumTransactionRequest, TransactionState,
+    QuorumTransactionRequest,
+    TransactionState,
     VaultManager,
     VaultRole
 } from "./sdk_prototype/vault_manager";
 import {execute} from "../util/call.util";
 import {getTransactionByIdFromGetAllTrs} from "./member_transactions.test";
 import {expect} from "chai";
+import {principalToAddress} from "ictool";
 
 require('./bigintExtension.js');
 
 describe("Batch Transactions", () => {
     let canister_id;
     let admin_identity = getIdentity("87654321876543218765432187654321")
+    let admin_identity2 = getIdentity("87654321876543218765432187654322")
+    let admin_identity3 = getIdentity("87654321876543218765432187654323")
     let manager: VaultManager;
+    let manager2: VaultManager;
     before(async () => {
         DFX.INIT();
         DFX.USE_TEST_ADMIN();
         await console.log(execute(`./test/resource/vault.sh`))
         canister_id = DFX.GET_CANISTER_ID("vault");
         manager = new VaultManager();
+        manager2 = new VaultManager();
         await manager.init(canister_id, admin_identity, true);
+        await manager2.init(canister_id, admin_identity2, true);
     });
 
     after(() => {
@@ -30,7 +38,7 @@ describe("Batch Transactions", () => {
     });
 
     it("Request batch transaction - rejected", async function () {
-        let memberCreate =  new MemberCreateTransactionRequest("memberAddress", "memberName", VaultRole.ADMIN);
+        let memberCreate = new MemberCreateTransactionRequest("memberAddress", "memberName", VaultRole.ADMIN);
         let quorumTransactionRequest = new QuorumTransactionRequest(3);
         let batchUid = "someRandomGeneratedUID_1"
         memberCreate.batch_uid = batchUid
@@ -47,7 +55,7 @@ describe("Batch Transactions", () => {
     });
 
     it("Request batch transaction - executed", async function () {
-        let memberCreate =  new MemberCreateTransactionRequest("memberAddress", "memberName", VaultRole.ADMIN);
+        let memberCreate = new MemberCreateTransactionRequest(principalToAddress(admin_identity2.getPrincipal() as any), "memberName", VaultRole.ADMIN);
         let quorumTransactionRequest = new QuorumTransactionRequest(2);
         let batchUid = "someRandomGeneratedUID_2"
         memberCreate.batch_uid = batchUid
@@ -58,11 +66,42 @@ describe("Batch Transactions", () => {
         let tr1 = await getTransactionByIdFromGetAllTrs(manager, res[0].id)
         let tr2 = await getTransactionByIdFromGetAllTrs(manager, res[1].id)
         expect(state.members.length).eq(2)
-        expect(state.quorum).eq(2)
+        expect(state.quorum.quorum).eq(2)
         expect(tr1.state).eq(TransactionState.Executed)
         expect(tr2.state).eq(TransactionState.Executed)
     });
 
 
+    it("Request batch transaction - with approves executed", async function () {
+        let memberCreate = new MemberCreateTransactionRequest(principalToAddress(admin_identity3.getPrincipal() as any), "memberName", VaultRole.ADMIN);
+        let quorumTransactionRequest = new QuorumTransactionRequest(3);
+        let batchUid = "someRandomGeneratedUID_3"
+        memberCreate.batch_uid = batchUid
+        quorumTransactionRequest.batch_uid = batchUid
+        let res = await manager.requestTransaction([memberCreate, quorumTransactionRequest])
+        await manager.execute()
+        let tr1 = await getTransactionByIdFromGetAllTrs(manager, res[0].id)
+        let tr2 = await getTransactionByIdFromGetAllTrs(manager, res[1].id)
+        expect(tr1.state).eq(TransactionState.Pending)
+        expect(tr2.state).eq(TransactionState.Blocked)
+        let approve1: ApproveRequest = {
+            tr_id: tr1.id,
+            state: TransactionState.Approved
+        }
+        let approve2: ApproveRequest = {
+            tr_id: tr2.id,
+            state: TransactionState.Approved
+        }
+        await manager2.approveTransaction([approve1, approve2])
+        await manager.execute();
+        tr1 = await getTransactionByIdFromGetAllTrs(manager, res[0].id)
+        tr2 = await getTransactionByIdFromGetAllTrs(manager, res[1].id)
+
+        let state = await manager.redefineState();
+        expect(state.members.length).eq(3)
+        expect(state.quorum.quorum).eq(3)
+        expect(tr1.state).eq(TransactionState.Executed)
+        expect(tr2.state).eq(TransactionState.Executed)
+    });
 })
 
