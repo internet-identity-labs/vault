56323b38ebb51908329afc8739b0bc99d0cd2dcd
olsemeno <sendtosemenov@gmail.com>
2023-12-07 11:09:35 +0100

    Transfer

--- src/vault/src/transaction/transaction_builder.rs
+++ src/vault/src/transaction/transaction_builder.rs
@@ -3,6 +3,7 @@ use crate::enums::TransactionState::Blocked;
 use crate::security_service::verify_caller;
 use crate::transaction::transaction::ITransaction;
 
+//TODO reorganize this trait
 pub trait TransactionBuilder {
     fn build_dyn_transaction(&mut self, state: TransactionState) -> Box<dyn ITransaction>;
     fn build(&mut self) -> Box<dyn ITransaction> {
