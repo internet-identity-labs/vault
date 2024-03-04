df958047beff32eff663aa476ae86bbe8d2706e8
Dmitrii <105656449+dmitriiIdentityLabs@users.noreply.github.com>
2024-03-04 17:00:57 +0400

    Subscription Integrate Stripe payment workflow [sc-11137] (#62)

--- test/vault_manager/sdk/ochestrator.ts
+++ test/vault_manager/sdk/ochestrator.ts
@@ -2,10 +2,11 @@ import {getActor} from "../../util/deployment.util";
 import {idlFactory} from "./vm_idl";
 import {SignIdentity} from "@dfinity/agent";
 import {Conf, Result, VaultCanister, VaultType} from "./vm";
+import {Principal} from "@dfinity/principal";
 
-export async function createCanister(canister_id: string, identity: SignIdentity, transactionBlock: BigInt, vaultType: Array<VaultType>) {
+export async function createCanister(canister_id: string, identity: SignIdentity, transactionBlock: BigInt, vaultType: Array<VaultType>, owner: Array<Principal> = []) {
     let actor = await getActor(canister_id, identity, idlFactory);
-    let result = await actor.create_canister_call(transactionBlock, vaultType) as Result;
+    let result = await actor.create_canister_call(transactionBlock, vaultType, owner) as Result;
     // @ts-ignore
     if (result.Err) {
         // @ts-ignore
