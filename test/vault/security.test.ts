c476c8fd6cea02ded59dbbaac5428315e5ef1f15
olsemeno <33149496+olsemeno@users.noreply.github.com>
2024-04-30 12:51:28 +0300

    Explicit security check (#88)
    * Add explicit security guard
    
    * Add explicit security guard
    
    * Add explicit security guard

--- test/vault/security.test.ts
+++ test/vault/security.test.ts
@@ -13,22 +13,25 @@ describe("Security ", () => {
     let canister_id;
     const admin = getIdentity("87654321876543218765432187654321");
     const admin2 = getIdentity("87654321876543218765432187654323");
+    const member = getIdentity("87654321876543218765432187654320");
+    const notRegistered = getIdentity("87654321876543218765432187654328");
     let adminManager: VaultManager;
     let adminManager2: VaultManager;
     let memberManager: VaultManager;
+    let notRegisteredManager: VaultManager;
     before(async () => {
         DFX.INIT();
         DFX.USE_TEST_ADMIN();
-        await console.log(execute(`./test/resource/ledger.sh`))
         await console.log(execute(`./test/resource/vault.sh`))
-        const member = getIdentity("87654321876543218765432187654320");
         canister_id = DFX.GET_CANISTER_ID("vault");
         adminManager = new VaultManager(canister_id, admin);
         adminManager2 = new VaultManager(canister_id, admin2);
         memberManager = new VaultManager(canister_id, member);
+        notRegisteredManager = new VaultManager(canister_id, notRegistered);
         await adminManager.resetToLocalEnv();
         await adminManager2.resetToLocalEnv();
         await memberManager.resetToLocalEnv();
+        await notRegisteredManager.resetToLocalEnv();
         await adminManager.execute()
         await requestCreateMemberTransaction(adminManager, principalToAddress(admin2.getPrincipal() as any), "Admin2", VaultRole.ADMIN)
         await requestCreateMemberTransaction(adminManager, principalToAddress(member.getPrincipal() as any), "Member", VaultRole.MEMBER)
@@ -72,8 +75,29 @@ describe("Security ", () => {
             await adminManager2.approveTransaction([{trId: id, state: TransactionState.Approved}])
             await memberManager.execute()
             await adminManager2.approveTransaction([{trId: id, state: TransactionState.Approved}])
+            fail("")
         } catch (e) {
             expect(e.message).contains("Transaction is immutable")
+        }
+    });
+
+    it("Request register transaction from the notregistered", async function () {
+        try {
+            await requestCreateMemberTransaction(notRegisteredManager, "testAddress2", "testName", VaultRole.MEMBER)
+            fail("")
+        } catch (e) {
+            expect(e.message).contains("Not registered")
+        }
+    });
+
+    it("Request approve transaction from the notregistered", async function () {
+        let t = await requestCreateMemberTransaction(adminManager, "testAddress2", "testName", VaultRole.MEMBER)
+        id = t[0].id;
+        try {
+            await notRegisteredManager.approveTransaction([{trId: id, state: TransactionState.Approved}])
+            fail("")
+        } catch (e) {
+            expect(e.message).contains("Not registered")
         }
     });
 
